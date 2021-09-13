79992ecb29745a35b3687918e14709815e6b248b
oracle727 <ec2-user@ip-172-31-83-77.ec2.internal>
2021-09-13 08:42:34 +0000

    InitialCommit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+Refile.secret_key = '495955d643469b6aad34590c64cb723d5846dfb393a96a634122bfc954ea225d018bbafbcc84bc0d7ffb18edd19cc91b41e4af3ddafa4521f5aaff9c323c5dc8'
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
